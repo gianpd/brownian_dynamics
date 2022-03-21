505c417fcd4dc5d80e2d2735a8136f5f8e6e7ec4
GianP <gian.pio.domiziani@accenture.com>
2022-03-21 16:27:12 +0100

    added possibility to include or exclude the random force component.

--- simulation.py
+++ simulation.py
@@ -15,11 +15,12 @@ def kick_step_propagator(t, p, f=None):
 
 def drift_step_propagator(t, r, p, box):
     r = r + t * (p / box)
-    r = r - np.rint(r) # periodic bounderies
+    #r = r - np.rint(r) # periodic bounderies
+    r = r % box
     return r
     
 
-def random_step_propagator(t, p, gamma, T, n):
+def random_step_propagator(t, p, gamma, T, n, random=True):
     """friction and random contributions propagator.
     t is the time over which to propagate (typically dt).
     gamma is the friction coefficient, T is the temperature, n is the number of particles
@@ -27,16 +28,21 @@ def random_step_propagator(t, p, gamma, T, n):
     x = gamma * t
     c = 1 - np.exp(-2 * x) if x > 0.0001 else np.polyval([-2/3, 4/3, -2.0, 2.0, 0.0], x)
     c = np.sqrt(c)
-    p = p * np.exp(-x) + c * np.sqrt(T) * np.random.randn(n, 3)
-    return p
+    if random:
+        p = p * np.exp(-x) + c * np.sqrt(T) * np.random.randn(n, 3)
+        return p
+    else:
+        # just friction
+        p = p * np.exp(-x)
+        return p
 
 
-def main(t, r, p, gamma, box, T, n, nsteps, f=None):
+def main(t, r, p, gamma, box, T, n, nsteps, f=None, random=True):
     rs, ps = [], []
     for i in range(nsteps):
         p = kick_step_propagator(t * 0.5, p, f)
         r = drift_step_propagator(t * 0.5, r, p, box)
-        p = random_step_propagator(t, p, gamma, T, n)
+        p = random_step_propagator(t, p, gamma, T, n, random=random)
         rs.append(drift_step_propagator(t * 0.5, r, p, box))
         ps.append(kick_step_propagator(t * 0.5, p, f))
     return rs, ps
@@ -49,9 +55,9 @@ def update_lines(num, walks, lines):
         line.set_3d_properties(walk[:num, 2])
     return lines
 
-def save_gif(dt, r, p, gamma, box, T, n, num_steps, f=None, lw=2):
+def save_gif(fname, dt, r, p, gamma, box, T, n, num_steps, f=None, lw=2, random=True):
     # Data: simulation of a 3D brownian dynamics of N particles
-    rs, _ = main(dt, r, p, gamma, box, T, n, num_steps, f)
+    rs, _ = main(dt, r, p, gamma, box, T, n, num_steps, f, random=random)
     rs = np.asarray(rs).reshape((n, num_steps, 3))
     
     # Attaching 3D axis to the figure
@@ -62,17 +68,17 @@ def save_gif(dt, r, p, gamma, box, T, n, num_steps, f=None, lw=2):
     lines = [ax1.plot([], [], [], lw=lw)[0] for _ in rs]
     
     # Setting the axes properties
-    ax1.set(xlim3d=(-1, 1), xlabel='X')
-    ax1.set(ylim3d=(-1, 1), ylabel='Y')
-    ax1.set(zlim3d=(-1, 1), zlabel='Z')
+    ax1.set(xlim3d=(0, 1), xlabel='X')
+    ax1.set(ylim3d=(0, 1), ylabel='Y')
+    ax1.set(zlim3d=(0, 1), zlabel='Z')
     
     # Creating the Animation object
     anim = animation.FuncAnimation(
-        fig, update_lines, num_steps, fargs=(rs, lines), interval=120)
+        fig, update_lines, num_steps, fargs=(rs, lines), interval=520)
     
     writergif = animation.PillowWriter(fps=30)
     f = f if f else 0.0
-    anim.save(f'sim_G{gamma}_T{T}_N{n}_F{f}.gif', writer=writergif, dpi=150)
+    anim.save(f'gifs/{fname}_G{gamma}_T{T}_N{n}_F{f}_dt{dt}.gif', writer=writergif, dpi=150)
     plt.close()
 
 
@@ -107,10 +113,10 @@ if __name__ == '__main__':
         '--N', action='store', required=False, default=1, type=int,
         help='Number of particles (defalt value = 1)'
     )
-    # parser.add_argument(
-    #     '--fname', action='store', required=True, type=str,
-    #     help='file name of the final gif simulation (without extensions).'
-    # )
+    parser.add_argument(
+        '--fname', action='store', required=False, default='sim', type=str,
+        help='file name of the final gif simulation (without extensions).'
+    )
     parser.add_argument(
         '--lw', action='store', required=False, type=float,
         help='Line Width dimension.'
@@ -118,6 +124,10 @@ if __name__ == '__main__':
     parser.add_argument(
         '--F', action='store', required=False, type=float,
         help='External Force magnitude.'
+    )
+    parser.add_argument(
+        '--exclude_random', action='store_false', required=False, default=True,
+        help='If exluding the random force component.'
     )
 
 
@@ -130,9 +140,10 @@ if __name__ == '__main__':
     T = args.T
     N = args.N
     num_steps = args.num_steps
-    # fname = args.fname
+    fname = args.fname
     lw = args.lw
     F = args.F
+    random = args.exclude_random
     
-    save_gif(dt, r, p, gamma, box, T, N, num_steps, f=F, lw=lw)
+    save_gif(fname, dt, r, p, gamma, box, T, N, num_steps, f=F, lw=lw, random=random)
 
