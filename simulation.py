3e75aa3e0fbb77ff78e6e66b7b5c63c8738e7e1f
GianP <gian.pio.domiziani@accenture.com>
2022-03-22 22:13:48 +0100

    num steps and time step size changed.

--- simulation.py
+++ simulation.py
@@ -1,26 +1,26 @@
 import argparse
 
 import numpy as np
-np.random.seed(12345)
+np.random.seed(123)
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
 def kick_step_propagator(t, p, f=None):
-    """t is the time step (usually dt/2); p is the momentum which must be propagated, f the external force"""
+    """t is the time step (usually dt/2); p is the momentum which must be propagated """
     if f:
         return p + t * f
     else:
-        return p
+        return p 
 
-def drift_step_propagator(t, r, p, box):
+def drift_step_propagator(t, r, p, box, periodic=True):
     r = r + t * (p / box)
     #r = r - np.rint(r) # periodic bounderies
-    r = r % box
+    r = r % box if periodic else r
     return r
     
 
-def random_step_propagator(t, p, gamma, T, n, random=True):
+def random_step_propagator(t, p, gamma, T, n):
     """friction and random contributions propagator.
     t is the time over which to propagate (typically dt).
     gamma is the friction coefficient, T is the temperature, n is the number of particles
@@ -28,27 +28,22 @@ def random_step_propagator(t, p, gamma, T, n, random=True):
     x = gamma * t
     c = 1 - np.exp(-2 * x) if x > 0.0001 else np.polyval([-2/3, 4/3, -2.0, 2.0, 0.0], x)
     c = np.sqrt(c)
-    if random:
-        p = p * np.exp(-x) + c * np.sqrt(T) * np.random.randn(n, 3)
-        return p
-    else:
-        # the system will not reach any equilibrium state because 
-        # at long-time its velocity will be zero, while the equipartition theorem requires:
-        # <v**2> \sim K_BT. The random force allows to reach the equilibrium state (fluctuation-dissipation theorem)
-        p = p * np.exp(-x)
-        return p
+    p = p * np.exp(-x) + c * np.sqrt(T) * np.random.randn(n, 3)
+    return p
 
 
-def main(t, r, p, gamma, box, T, n, nsteps, f=None, random=True):
+def main(t, r, p, gamma, box, T, n, nsteps, f=None, periodic=True):
     rs, ps = [], []
+    #r = r / box
     for i in range(nsteps):
         p = kick_step_propagator(t * 0.5, p, f)
-        r = drift_step_propagator(t * 0.5, r, p, box)
-        p = random_step_propagator(t, p, gamma, T, n, random=random)
-        rs.append(drift_step_propagator(t * 0.5, r, p, box))
+        r = drift_step_propagator(t * 0.5, r, p, box, periodic=periodic)
+        p = random_step_propagator(t, p, gamma, T, n)
+        rs.append(drift_step_propagator(t * 0.5, r, p, box, periodic=periodic))
         ps.append(kick_step_propagator(t * 0.5, p, f))
     return rs, ps
 
+    
 
 def update_lines(num, walks, lines):
     for line, walk in zip(lines, walks):
@@ -57,9 +52,9 @@ def update_lines(num, walks, lines):
         line.set_3d_properties(walk[:num, 2])
     return lines
 
-def save_gif(fname, dt, r, p, gamma, box, T, n, num_steps, f=None, lw=2, random=True):
+def save_gif(fname, dt, r, p, gamma, box, T, n, num_steps, f=None, lw=2, periodic=True):
     # Data: simulation of a 3D brownian dynamics of N particles
-    rs, _ = main(dt, r, p, gamma, box, T, n, num_steps, f, random=random)
+    rs, _ = main(dt, r, p, gamma, box, T, n, num_steps, f, periodic=periodic)
     rs = np.asarray(rs).reshape((n, num_steps, 3))
     
     # Attaching 3D axis to the figure
@@ -70,17 +65,17 @@ def save_gif(fname, dt, r, p, gamma, box, T, n, num_steps, f=None, lw=2, random=
     lines = [ax1.plot([], [], [], lw=lw)[0] for _ in rs]
     
     # Setting the axes properties
-    ax1.set(xlim3d=(0, 1), xlabel='X')
-    ax1.set(ylim3d=(0, 1), ylabel='Y')
-    ax1.set(zlim3d=(0, 1), zlabel='Z')
+    #ax1.set(xlim3d=(0, box), xlabel='X')
+    #ax1.set(ylim3d=(0, box), ylabel='Y')
+    #ax1.set(zlim3d=(0, box), zlabel='Z')
     
     # Creating the Animation object
     anim = animation.FuncAnimation(
-        fig, update_lines, num_steps, fargs=(rs, lines), interval=520)
+        fig, update_lines, num_steps, fargs=(rs, lines), interval=10)
     
     writergif = animation.PillowWriter(fps=30)
     f = f if f else 0.0
-    anim.save(f'gifs/{fname}_G{gamma}_T{T}_N{n}_F{f}_dt{dt}.gif', writer=writergif, dpi=150)
+    anim.save(f'gifs/{fname}_G{gamma}_T{T}_N{n}_F{f}_dt{dt}_box{box}.gif', writer=writergif, dpi=150)
     plt.close()
 
 
@@ -96,8 +91,8 @@ if __name__ == '__main__':
         default=(0.0, 0.0)
     )
     parser.add_argument(
-        '--box_dim', required=False, type=float, action='store', default=1.0,
-        help='Box dimensions (default=1.0)'
+        '--box_dim', required=False, type=float, action='store', default=3.0,
+        help='Box dimensions (default=3.0)'
     )
     parser.add_argument(
         '--num_steps', action='store', required=False, type=int, default=1000,
@@ -127,25 +122,19 @@ if __name__ == '__main__':
         '--F', action='store', required=False, type=float,
         help='External Force magnitude.'
     )
-    parser.add_argument(
-        '--exclude_random', action='store_false', required=False, default=True,
-        help='If exluding the random force component.'
-    )
 
 
     args = parser.parse_args()
     
     dt = args.dt
+    num_steps = args.num_steps
     r, p = args.IC
     box = args.box_dim
     gamma = args.gamma
     T = args.T
     N = args.N
-    num_steps = args.num_steps
     fname = args.fname
     lw = args.lw
     F = args.F
-    random = args.exclude_random
-    
-    save_gif(fname, dt, r, p, gamma, box, T, N, num_steps, f=F, lw=lw, random=random)
 
+    save_gif(fname, dt, r, p, gamma, box, T, N, num_steps, F, lw)
